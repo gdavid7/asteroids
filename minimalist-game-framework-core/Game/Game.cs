c646269029f858ec934b6ae19e1485843abe20a6
Nindroz <mantha.hitesh@gmail.com>
2022-11-15 08:59:11 -0800

    basic sprite to work with

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -6,11 +6,18 @@ class Game
     public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
 
+    Texture ship = Engine.LoadTexture("ship.png");
+
+
     public Game()
     {
+
     }
 
     public void Update()
     {
+
+        Engine.DrawTexture(ship, new Vector2(100, 100), size: new Vector2(100, 100));
+
     }
 }
